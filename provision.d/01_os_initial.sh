c3048afe9ccba84cf2d94d7adf9ce50f43f54a71
Peter Rossbach <peterrossbach@me.com>
2014-10-02 20:09:20 +0200

    fix shell shocker

--- provision.d/01_os_initial.sh
+++ provision.d/01_os_initial.sh
@@ -1,6 +1,7 @@
 killall -9 apt-get >/dev/null 2>&1
 
 apt-get update -yqq
-
+# shell shocker fix
+apt-get install --only-upgrade bash
 # even if update fails, continue with clean exit code
 /bin/true
