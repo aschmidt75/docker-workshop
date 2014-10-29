a88a0f87adfb31752d3c1858f822fe05c599762e
Peter Rossbach <peterrossbach@me.com>
2014-10-29 06:51:59 +0100

    add microservice introduction

--- docker.d/slides/microservice/run.sh
+++ docker.d/slides/microservice/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -ti -p 8000:8000 -v `pwd`/images:/opt/presentation/images -v `pwd`:/opt/presentation/md rossbachp/presentation
+docker run -d -ti -p 8003:8000 -v `pwd`/images:/opt/presentation/images -v `pwd`/build:/build -v `pwd`:/opt/presentation/lib/md rossbachp/presentation
