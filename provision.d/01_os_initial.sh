60cbc8282520ed0aea4a9e2ac58d78676792f8b5
Peter Rossbach <peterrossbach@me.com>
2014-11-01 20:23:31 +0100

    fix zip installation

--- provision.d/01_os_initial.sh
+++ provision.d/01_os_initial.sh
@@ -2,7 +2,7 @@ killall -9 apt-get >/dev/null 2>&1
 
 apt-get update -yqq
 # shell shocker fix
-apt-get install --only-upgrade bash
-apt-get install zip
+apt-get install -y --only-upgrade bash
+apt-get install -yqq zip
 # even if update fails, continue with clean exit code
 /bin/true
