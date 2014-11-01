47bd419a28c1f6a1a576306f9a4bc96a6869fdf3
Peter Rossbach <peterrossbach@me.com>
2014-11-01 23:48:55 +0100

    add insecure option to docker daemon

--- puppet.d/modules/docker_host/manifests/install.pp
+++ puppet.d/modules/docker_host/manifests/install.pp
@@ -6,5 +6,6 @@ class docker_host::install {
     version         => '1.3.1',
     manage_kernel   => false,
     socket_bind     => 'unix:///var/run/docker.sock',
+    extra_parameters => [ "--insecure-registry", "127.0.0.1:5000" ],
   }
 }
