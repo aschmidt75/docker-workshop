4653437117dfb626c242f751dc03c806d70c3306
Andreas Schmidt <andreas.schmidt@cassini.de>
2014-11-08 11:00:21 +0100

    added pwd to vagrant user

--- puppet.d/modules/docker_host/manifests/config.pp
+++ puppet.d/modules/docker_host/manifests/config.pp
@@ -2,11 +2,13 @@ include 'docker'
 
 # configure the docker host:
 # - make vagrant use the socket
+# - patch in default password (vagrant), so users can log in on tty
 # - pull image
 class docker_host::config {
   # - give vagrant user(group) access to docker socket
   user { 'vagrant':
-    groups   => 'docker'
+    groups    => 'docker',
+    password => '$6$PItmuCw3$GhVuRvitKtZOgufjE5e60pbOQvwi89We8AXYo7m1ARIecED7sjHN1q4Bqgru4S6qrFPQZ46jkGln.XiHzgK4P.'
   }
 
   # disable UFW
